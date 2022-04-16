af3c528e6c0ada982667ce56f455a186e1a0ab3d
JakeTM9 <wardegjn@mail.uc.edu>
2022-04-16 19:02:42 -0400

    data filtering for character, season, and episode

--- js/main.js
+++ js/main.js
@@ -1,45 +1,96 @@
-let characters = []
-let episodes = []
-
+//see examples at the bottom
 d3.csv('data/himym-dialogues.csv')
 .then(data => {
-    //default processing for season/episode data than can be re-run on a specific character selection
-    data.forEach(d => {
-        //add a conditional for character filtering here
-        if (episodes.some(item => item.season === d.season && item.episode == d.episode)){
-            episodes.some(function(item){
-                if (item.season === d.season && item.episode == d.episode){
-                    item.numScenes = d.scene;
-                    if(!item.characters.includes(d.character)){
-                        item.characters.push(d.character);
-                    }
-                    return true;
+
+    function filterData(character, season, episode, data) {
+        let characters = []
+        let episodes = []
+        data.forEach(d => {
+            if ((d.character === character || character === null) && (d.season === season || season === null) && (d.episode === episode || episode === null)){
+                if (episodes.some(e => e.season === d.season && e.episode == d.episode)){
+                    episodes.some(function(e){
+                        if (e.season === d.season && e.episode == d.episode){
+                            e.numScenes = d.scene;
+                            if(e.characters.some(character => character.name === d.character)){
+                                e.characters.some(function(c){
+                                    if (c.name === d.character){
+                                        c.numLines += 1;
+                                        return true;
+                                    }
+                                });
+                            }
+                            else{
+                                e.characters.push({"name" : d.character, "numLines" : 1});
+                            }
+                            return true;
+                        }
+                    });
                 }
-            });
-        }
-        else{
-            episodes.push({"season" : d.season, "episode" : d.episode, "numScenes" : d.scene, "characters" : [d.character]});
-        }
-    });
-    //default processing for character data than can be re-run on a specific season/episode selection
-    data.forEach(d => {
-        //add an conditioanl for season/episode filtering here
-        if (characters.some(item => item.name === d.character)){
-            characters.some(function(item){
-                if (item.name === d.character){
-                    item.numLines += 1;
-                    return true;
+                else{
+                    episodes.push({"season" : d.season, "episode" : d.episode, "numScenes" : d.scene, "characters" : [{"name" : d.character, "numLines" : 1}]});
                 }
-            });
-        }
-        else{
-            characters.push({"name" : d.character, "numLines" : 1});
-        }
-    });
-    console.log(data)
+            }
+        });
+        episodes.forEach(e => {
+            e.characters.forEach(d => {
+                if (characters.some(c => c.name === d.name)){
+                    characters.some(function(c){
+                        if (c.name === d.name){
+                            c.numLines += d.numLines;
+                            return true;
+                        }
+                    });
+                }
+                else{
+                    characters.push({"name" : d.name, "numLines" : d.numLines});
+                }
+            })
+        });
+        return [episodes , characters];
+    }
+    
+    let result = filterData(null, null, null, data);
+    let episodes = result[0];
+    let characters = result[1];
+    console.log("data below is relevent to whole series")
+    console.log(episodes);
     console.log(characters);
-    console.log(episodes)
-    //TODO: filtering examples below
+    
+    //Examples of filtering on selection
+
+    //selecting a character -> which episodes/seasons they appear in, how much they speak in these episodes/seasons
+
+    result = filterData("Ted", null, null, data);
+    episodes = result[0];
+    characters = result[1];
+    console.log("data below is only going to relevent to Ted")
+    console.log(episodes);
+    console.log(characters);
+
+    //selecting a episode or a season -> which characters eppeared, who spoke most often etc
+    
+    result = filterData(null, "1", null, data);
+    episodes = result[0];
+    characters = result[1];
+    console.log("data below is only going to relevent to Season 1")
+    console.log(episodes);
+    console.log(characters);
+
+    result = filterData(null, null, "8", data);
+    episodes = result[0];
+    characters = result[1];
+    console.log("data below is only going to relevent to 8th episodes of seasons")
+    console.log(episodes);
+    console.log(characters);
+
+    //all filters applied
+
+    result = filterData("Lily", "3", "4", data);
+    episodes = result[0];
+    characters = result[1];
+    console.log("data below is only going to relevent to Lily in Season 3 episode 4")
+    console.log(episodes);
+    console.log(characters);
 
 }).catch(error => console.error(error));
 
