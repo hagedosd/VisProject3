b026a8626101a779772f439f426a941a9770e4d3
JakeTM9 <wardegjn@mail.uc.edu>
2022-04-16 17:42:34 -0400

    episode data processing

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 let characters = []
+let episodes = []
 
-
-
-
-
-
-//default processing for character data than can be re-done on a specific season/episode selection
 d3.csv('data/himym-dialogues.csv')
 .then(data => {
+    //default processing for season/episode data than can be re-run on a specific character selection
     data.forEach(d => {
-        //add an if with season/episode filtering here
+        //add a conditional for character filtering here
+        if (episodes.some(item => item.season === d.season && item.episode == d.episode)){
+            episodes.some(function(item){
+                if (item.season === d.season && item.episode == d.episode){
+                    item.numScenes = d.scene;
+                    if(!item.characters.includes(d.character)){
+                        item.characters.push(d.character);
+                    }
+                    return true;
+                }
+            });
+        }
+        else{
+            episodes.push({"season" : d.season, "episode" : d.episode, "numScenes" : d.scene, "characters" : [d.character]});
+        }
+    });
+    //default processing for character data than can be re-run on a specific season/episode selection
+    data.forEach(d => {
+        //add an conditioanl for season/episode filtering here
         if (characters.some(item => item.name === d.character)){
             characters.some(function(item){
                 if (item.name === d.character){
@@ -24,6 +38,9 @@ d3.csv('data/himym-dialogues.csv')
     });
     console.log(data)
     console.log(characters);
+    console.log(episodes)
+    //TODO: filtering examples below
+
 }).catch(error => console.error(error));
 
 
