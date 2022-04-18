e910f0a1796cac68dbd5f4414c49c9def6fc3965
hagedosd <hagedosd@mail.uc.edu>
2022-04-18 09:23:00 -0400

    Removed commented out code from script-processor.py. Disabled the script since the csv it can generate has already been generated once.

--- data/python/script-processer.py
+++ data/python/script-processer.py
@@ -4,43 +4,21 @@ import re
 from pathlib import Path
 
 txtFileDir = 'data/transcripts'
-# nameFilter = re.compile(r"^([\w\s]+: ?.+)|^\(.+\)[^\S\r\n]([\w\s]+: .+)|^(?:\(.+\)|\[.+\])([\w]+: .+)|([\w]+).+(: .+)|\*{2}Season-(\d)|\*{2}Episode-(\d)|(Scene)")
+
+# This regex attempts to capture character names and dialouge in a more consistent way
 nameFilter = re.compile(r"^([\w\s]+: ?.+)|^\(.+\)[^\S\r\n]([\w\s]+: .+)|^(?:\(.+\)|\[.+\])([\w]+: .+)|([\w]+).+(: .+)")
 
-# ##################
-# print(f'script-processor.py is dsiabled with exit().')
-# exit()
-# ##################
+##################
+# Added to prevent script from running unnecessarily
+# This script only needs to run once to generate himym-dialogues.csv
+print(f'script-processor.py is dsiabled with exit().')
+exit()
+##################
 
 # Initialize list of line dicts
 lineDictList = []
 
 # For each transcript file
-# for filename in os.listdir(txtFileDir):
-#     file = os.path.join(txtFileDir, filename)
-#     with open(file) as txtFile:
-#         lines = txtFile.read().split('\n')
-#         season = 0
-#         episode = 0
-#         scene = 0
-#         for line in lines:
-#             if '**Season' in line:
-#                 season = int(line.split('-')[1])
-#             if '**Episode' in line:
-#                 episode = int(line.split('-')[1])
-#             if line.startswith('Scene'):
-#                 scene += 1
-#             if ':' in line and '[' not in line:
-#                 # Correct for the episodes that don't specify first scene is starting
-#                 if scene == 0:
-#                     scene = 1
-#                 lineSplit = line.split(':')
-#                 character = lineSplit[0]
-#                 dialogue = lineSplit[1]
-#                 lineDict = {'season': season, 'episode': episode, 'scene': scene, 'character': character, 'dialogue': dialogue}
-#                 lineDictList.append(lineDict)
-
-# file = os.path.join(txtFileDir, "S1-Ep1.txt")
 for filename in os.listdir(txtFileDir):
     file = os.path.join(txtFileDir, filename)
     with open(file) as textFile:
@@ -48,8 +26,6 @@ for filename in os.listdir(txtFileDir):
         season = 0
         episode = 0
         scene = 0
-        linecount = 0
-        nameAndDialouge = [None]
         for line in lines:
             result = re.search(nameFilter, line)
             if '**Season' in line:
@@ -58,26 +34,26 @@ for filename in os.listdir(txtFileDir):
                 episode = int(line.split('-')[1])
             elif line.startswith('Scene'):
                 scene += 1
+            
+            # This executes when the regex matches something in the current line
             elif result:
-                linecount += 1
                 things = result.groups()
                 importantThings = list(filter(None, things))
                 if scene == 0:
                     scene = 1
+                # If the regex made 2 or more captures join them into one element
                 if (len(importantThings)>1):
                     joinedThings = "".join(importantThings)
-                    # print("Joined important things:", joinedThings)
+                # When there is one capture move it into joinedThings so a split is done on only one variable
                 else:
                     joinedThings = importantThings[0]
-                    # print("Nonjoined things:", joinedThings)
+
+                # Split the name and dialouge using ":"
                 lineSplit = joinedThings.split(':')
-                # print("This is the captured data:", nameAndDialouge)
                 character = lineSplit[0]
                 dialogue = lineSplit[1]
                 lineDict = {'season': season, 'episode': episode, 'scene': scene, 'character': character, 'dialogue': dialogue}
                 lineDictList.append(lineDict)
-                # print(character, "says:", dialogue)
-                # print(character)
 
 
 # Write list to dataframe and save
